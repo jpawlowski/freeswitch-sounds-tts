b52dbdbcad7d6d1a36d0ca86241fb68cbb4b0fee
Julian Pawlowski <julian.pawlowski@gmail.com>
2013-02-11 23:41:10 +0100

    create tar.gz archives

--- make_sounds.sh
+++ make_sounds.sh
@@ -52,3 +52,12 @@ for FILE in $FILES; do
 done
 
 echo "Processing complete."
+
+echo -e "\nCreating archive files ...\n"
+
+rm -f ./freeswitch-sounds-de-de-callie-*.tar-gz
+
+cd ./output
+find . -name '16000' -type d | xargs tar cfpzv ../freeswitch-sounds-de-de-callie-16000-0.0.1.tar.gz
+find . -name '8000' -type d | xargs tar cfpzv ../freeswitch-sounds-de-de-callie-8000-0.0.1.tar.gz
+cd -
