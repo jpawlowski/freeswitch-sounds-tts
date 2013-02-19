7743033b0c8c2ec063641ce99577bf0f078d564d
Julian Pawlowski <julian.pawlowski@gmail.com>
2013-02-19 04:02:13 +0100

    initial support for Bing TTS

--- make_sounds.sh
+++ make_sounds.sh
@@ -2,114 +2,163 @@
 
 set -e
 
-if [[ ! -d ./input || ! curl || ! sox || ! mpg123 || ! perl ]]; then
-	echo -e "\nFATAL ERROR: Either one of the following errors occured:\n\n- input directory does not exist\n- curl/sox/perl are not installed\n"
+if [[ ! -d ./input || ! curl || ! sox || ! mpg123 || ! perl || ! php ]]; then
+	echo -e "\nFATAL ERROR: Either one of the following errors occured:\n\n- input directory does not exist\n- curl/sox/mpg123/perl/php are not installed\n"
 	exit 1
 fi
 
-GOOGLETTSURL="http://translate.google.com"
 MARYTTSURL="https://tts.profhost.eu"
 MARYTTSVOICESDE="bits1-hsmm bits3 bits3-hsmm dfki-pavoque-neutral dfki-pavoque-neutral-hsmm"
+BING_OAUTH_CLIENTID=""
+BING_OAUTH_CLIENTSECRET=""
+
 FILES="`cd ./input; find . -name *.txt`"
 
 # MARY TTS
-echo -e "\nNOW PROCESSING WITH ENGINE: MARY TTS\n"
-for FILE in $FILES; do
+if [[ x"$1" == x"marytts" || x"$1" == x"" ]]; then
+	echo -e "\nNOW PROCESSING WITH ENGINE: MARY TTS\n"
+	for FILE in $FILES; do
 
-	BASENAME="${FILE#.*/}"
-	FILENAME="${BASENAME%%.*}"
-	FILENAME_FLAT="${FILENAME#*/}"
-	LOCALE="${BASENAME%%/*}"
-	set +e
-	CHECK_TODO=`grep "#TODO" "./input/${BASENAME}"`
-	set -e
+		BASENAME="${FILE#.*/}"
+		FILENAME="${BASENAME%%.*}"
+		FILENAME_FLAT="${FILENAME#*/}"
+		LOCALE="${BASENAME%%/*}"
+		set +e
+		CHECK_TODO=`grep "#TODO" "./input/${BASENAME}"`
+		set -e
 
-	if [ x"${CHECK_TODO}" != x"" ]; then
-		echo "Ignoring ${FILENAME}"
-		continue;
-	fi
+		if [ x"${CHECK_TODO}" != x"" ]; then
+			echo "Ignoring ${FILENAME}"
+			continue;
+		fi
 
-	eval echo \${MARYTTSVOICES${LOCALE^^}} > fs_voices.tmp
-	VOICES="`cat fs_voices.tmp`"
-	rm -rf fs_voices.tmp
+		eval echo \${MARYTTSVOICES${LOCALE^^}} > fs_voices.tmp
+		VOICES="`cat fs_voices.tmp`"
+		rm -rf fs_voices.tmp
 
-	for VOICE in $VOICES; do
+		for VOICE in $VOICES; do
 	
-		OUTPUT_DIR="./output/${LOCALE}/tts/${VOICE}/${FILENAME_FLAT%/*}/16000"
-		OUTPUT_DIR8k="./output/${LOCALE}/tts/${VOICE}/${FILENAME_FLAT%/*}/8000"
+			OUTPUT_DIR="./output/${LOCALE}/tts/${VOICE}/${FILENAME_FLAT%/*}/16000"
+			OUTPUT_DIR8k="./output/${LOCALE}/tts/${VOICE}/${FILENAME_FLAT%/*}/8000"
+			OUTPUT_FILE="${OUTPUT_DIR}/${FILENAME##*/}.wav"
+			OUTPUT_FILE8k="${OUTPUT_DIR8k}/${FILENAME##*/}.wav"
+	
+			if [ -f "./input/${BASENAME}.maryxml" ]; then
+				INPUT_FILE="./input/${FILENAME}.maryxml"
+				INPUT_TYPE="RAWMARYXML"
+			else
+				INPUT_FILE="./input/${FILENAME}.txt"
+				INPUT_TYPE="TEXT"
+			fi
+	
+			if [ ! -f "${OUTPUT_FILE}" ]; then
+				echo "Processing ${FILENAME} for voice ${VOICE} ..."
+				mkdir -p "${OUTPUT_DIR}"
+				TEXT="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "`cat ${INPUT_FILE}`")"
+				curl -s "${MARYTTSURL}/process?INPUT_TEXT=${TEXT}&INPUT_TYPE=${INPUT_TYPE}&OUTPUT_TYPE=AUDIO&AUDIO=WAVE_FILE&LOCALE=${LOCALE}&VOICE=${VOICE}" > "${OUTPUT_FILE}"
+			fi
+	
+			if [[ ! -f "${OUTPUT_FILE8k}" && -f "${OUTPUT_FILE}" ]]; then
+				echo "Converting ${FILENAME} to 8kHz ..."
+				mkdir -p "${OUTPUT_DIR8k}"
+				sox -t wav "${OUTPUT_FILE}" -c1 -r8000 -b16 -e signed-integer "${OUTPUT_FILE8k}"
+			fi
+		done
+	done
+fi
+
+# GOOGLE TTS
+if [[ x"$1" == x"googletts" || x"$1" == x"" ]]; then
+	echo -e "\nNOW PROCESSING WITH ENGINE: GOOGLE TTS\n"
+	for FILE in $FILES; do
+
+		BASENAME="${FILE#.*/}"
+		FILENAME="${BASENAME%%.*}"
+		FILENAME_FLAT="${FILENAME#*/}"
+		LOCALE="${BASENAME%%/*}"
+		set +e
+		CHECK_TODO=`grep "#TODO" "./input/${BASENAME}"`
+		set -e
+
+		if [ x"${CHECK_TODO}" != x"" ]; then
+			echo "Ignoring ${FILENAME}"
+			continue;
+		fi
+
+		OUTPUT_DIR_TMP="./output.tmp/${LOCALE}/tts/google/${FILENAME_FLAT%/*}"
+		OUTPUT_DIR="./output/${LOCALE}/tts/google/${FILENAME_FLAT%/*}/16000"
+		OUTPUT_DIR8k="./output/${LOCALE}/tts/google/${FILENAME_FLAT%/*}/8000"
+		OUTPUT_FILE_TMP="${OUTPUT_DIR_TMP}/${FILENAME##*/}.mp3"
 		OUTPUT_FILE="${OUTPUT_DIR}/${FILENAME##*/}.wav"
 		OUTPUT_FILE8k="${OUTPUT_DIR8k}/${FILENAME##*/}.wav"
+		INPUT_FILE="./input/${FILENAME}.txt"
 	
-		if [ -f "./input/${BASENAME}.maryxml" ]; then
-			INPUT_FILE="./input/${FILENAME}.maryxml"
-			INPUT_TYPE="RAWMARYXML"
-		else
-			INPUT_FILE="./input/${FILENAME}.txt"
-			INPUT_TYPE="TEXT"
-		fi
-	
-		if [ ! -f "${OUTPUT_FILE}" ]; then
-			echo "Processing ${FILENAME} for voice ${VOICE} ..."
-			mkdir -p "${OUTPUT_DIR}"
+		if [ ! -f "${OUTPUT_FILE_TMP}" ]; then
+			echo "Processing ${FILENAME} ..."
+			mkdir -p "${OUTPUT_DIR_TMP}"
 			TEXT="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "`cat ${INPUT_FILE}`")"
-			curl -s "${MARYTTSURL}/process?INPUT_TEXT=${TEXT}&INPUT_TYPE=${INPUT_TYPE}&OUTPUT_TYPE=AUDIO&AUDIO=WAVE_FILE&LOCALE=${LOCALE}&VOICE=${VOICE}" > "${OUTPUT_FILE}"
+			curl -A "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17" -s "http://translate.google.com/translate_tts?tl=${LOCALE}&q=${TEXT}" > "${OUTPUT_FILE_TMP}"
+			sleep 1
 		fi
-	
+
+		if [[ ! -f "${OUTPUT_FILE}" && -f "${OUTPUT_FILE_TMP}" ]]; then
+			echo "Converting ${FILENAME} to 16kHz ..."
+			mkdir -p "${OUTPUT_DIR}"
+			mpg123 -q -w ${OUTPUT_FILE} ${OUTPUT_FILE_TMP}
+		fi
+
 		if [[ ! -f "${OUTPUT_FILE8k}" && -f "${OUTPUT_FILE}" ]]; then
 			echo "Converting ${FILENAME} to 8kHz ..."
 			mkdir -p "${OUTPUT_DIR8k}"
 			sox -t wav "${OUTPUT_FILE}" -c1 -r8000 -b16 -e signed-integer "${OUTPUT_FILE8k}"
 		fi
 	done
-done
+fi
 
-# GOOGLE TTS
-echo -e "\nNOW PROCESSING WITH ENGINE: GOOGLE TTS\n"
-for FILE in $FILES; do
+# BING TTS
+if [[ x"$1" == x"bingtts" || x"$1" == x"" ]]; then
+	if [[ x"${BING_OAUTH_CLIENTID}" != x"" && x"${BING_OAUTH_CLIENTSECRET}" != x"" ]]; then
+		echo -e "\nNOW PROCESSING WITH ENGINE: BING TTS\n"
+		
+		for FILE in $FILES; do
+			BASENAME="${FILE#.*/}"
+			FILENAME="${BASENAME%%.*}"
+			FILENAME_FLAT="${FILENAME#*/}"
+			LOCALE="${BASENAME%%/*}"
+			set +e
+			CHECK_TODO=`grep "#TODO" "./input/${BASENAME}"`
+			set -e
 
-	BASENAME="${FILE#.*/}"
-	FILENAME="${BASENAME%%.*}"
-	FILENAME_FLAT="${FILENAME#*/}"
-	LOCALE="${BASENAME%%/*}"
-	set +e
-	CHECK_TODO=`grep "#TODO" "./input/${BASENAME}"`
-	set -e
+			if [ x"${CHECK_TODO}" != x"" ]; then
+				echo "Ignoring ${FILENAME}"
+				continue;
+			fi
 
-	if [ x"${CHECK_TODO}" != x"" ]; then
-		echo "Ignoring ${FILENAME}"
-		continue;
-	fi
-
-	OUTPUT_DIR_TMP="./output.tmp/${LOCALE}/tts/google/${FILENAME_FLAT%/*}"
-	OUTPUT_DIR="./output/${LOCALE}/tts/google/${FILENAME_FLAT%/*}/16000"
-	OUTPUT_DIR8k="./output/${LOCALE}/tts/google/${FILENAME_FLAT%/*}/8000"
-	OUTPUT_FILE_TMP="${OUTPUT_DIR_TMP}/${FILENAME##*/}.mp3"
-	OUTPUT_FILE="${OUTPUT_DIR}/${FILENAME##*/}.wav"
-	OUTPUT_FILE8k="${OUTPUT_DIR8k}/${FILENAME##*/}.wav"
-	INPUT_FILE="./input/${FILENAME}.txt"
+			OUTPUT_DIR="./output/${LOCALE}/tts/bing/${FILENAME_FLAT%/*}/16000"
+			OUTPUT_DIR8k="./output/${LOCALE}/tts/bing/${FILENAME_FLAT%/*}/8000"
+			OUTPUT_FILE="${OUTPUT_DIR}/${FILENAME##*/}.wav"
+			OUTPUT_FILE8k="${OUTPUT_DIR8k}/${FILENAME##*/}.wav"
+			INPUT_FILE="./input/${FILENAME}.txt"
 	
-	if [ ! -f "${OUTPUT_FILE_TMP}" ]; then
-		echo "Processing ${FILENAME} ..."
-		mkdir -p "${OUTPUT_DIR_TMP}"
-		TEXT="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "`cat ${INPUT_FILE}`")"
-		curl -A "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17" -s "${GOOGLETTSURL}/translate_tts?tl=${LOCALE}&q=${TEXT}" > "${OUTPUT_FILE_TMP}"
-		sleep 1
+			if [ ! -f "${OUTPUT_FILE}" ]; then
+				echo "Processing ${FILENAME} ..."
+				BING_OAUTH_TOKEN="`php oauth_bingtts.php "${BING_OAUTH_CLIENTID}" "${BING_OAUTH_CLIENTSECRET}"`"
+				sleep 5
+				mkdir -p "${OUTPUT_DIR}"
+				TEXT="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "`cat ${INPUT_FILE}`")"
+				curl -A "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17" -H "Authorization: Bearer ${BING_OAUTH_TOKEN}" -s "http://api.microsofttranslator.com/V2/Http.svc/Speak?language=${LOCALE}&format=audio/wav&options=MaxQuality&appid=&text=${TEXT}" > "${OUTPUT_FILE}"
+			fi
+
+			if [[ ! -f "${OUTPUT_FILE8k}" && -f "${OUTPUT_FILE}" ]]; then
+				echo "Converting ${FILENAME} to 8kHz ..."
+				mkdir -p "${OUTPUT_DIR8k}"
+				sox -t wav "${OUTPUT_FILE}" -c1 -r8000 -b16 -e signed-integer "${OUTPUT_FILE8k}"
+			fi
+		done
+	else
+		echo "OAuth API credentials for BING missing. See http://msdn.microsoft.com/en-us/library/hh454950.aspx"
 	fi
-
-	if [[ ! -f "${OUTPUT_FILE}" && -f "${OUTPUT_FILE_TMP}" ]]; then
-		echo "Converting ${FILENAME} to 16kHz ..."
-		mkdir -p "${OUTPUT_DIR}"
-		mpg123 -q -w ${OUTPUT_FILE} ${OUTPUT_FILE_TMP}
-	fi
-
-	if [[ ! -f "${OUTPUT_FILE8k}" && -f "${OUTPUT_FILE}" ]]; then
-		echo "Converting ${FILENAME} to 8kHz ..."
-		mkdir -p "${OUTPUT_DIR8k}"
-		sox -t wav "${OUTPUT_FILE}" -c1 -r8000 -b16 -e signed-integer "${OUTPUT_FILE8k}"
-	fi
-done
-
-
+fi
 
 echo "Processing complete."
 
---
+++ oauth_bingtts.php
@@ -0,0 +1,79 @@
+<?php
+
+class AccessTokenAuthentication {
+    /*
+     * Get the access token.
+     *
+     * @param string $grantType    Grant type.
+     * @param string $scopeUrl     Application Scope URL.
+     * @param string $clientID     Application client ID.
+     * @param string $clientSecret Application client ID.
+     * @param string $authUrl      Oauth Url.
+     *
+     * @return string.
+     */
+    function getTokens($grantType, $scopeUrl, $clientID, $clientSecret, $authUrl){
+        try {
+            //Initialize the Curl Session.
+            $ch = curl_init();
+            //Create the request Array.
+            $paramArr = array (
+                 'grant_type'    => $grantType,
+                 'scope'         => $scopeUrl,
+                 'client_id'     => $clientID,
+                 'client_secret' => $clientSecret
+            );
+            //Create an Http Query.//
+            $paramArr = http_build_query($paramArr);
+            //Set the Curl URL.
+            curl_setopt($ch, CURLOPT_URL, $authUrl);
+            //Set HTTP POST Request.
+            curl_setopt($ch, CURLOPT_POST, TRUE);
+            //Set data to POST in HTTP "POST" Operation.
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $paramArr);
+            //CURLOPT_RETURNTRANSFER- TRUE to return the transfer as a string of the return value of curl_exec().
+            curl_setopt ($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            //CURLOPT_SSL_VERIFYPEER- Set FALSE to stop cURL from verifying the peer's certificate.
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            //Execute the  cURL session.
+            $strResponse = curl_exec($ch);
+            //Get the Error Code returned by Curl.
+            $curlErrno = curl_errno($ch);
+            if($curlErrno){
+                $curlError = curl_error($ch);
+                throw new Exception($curlError);
+            }
+            //Close the Curl Session.
+            curl_close($ch);
+            //Decode the returned JSON string.
+            $objResponse = json_decode($strResponse);
+            if ($objResponse->error){
+                throw new Exception($objResponse->error_description);
+            }
+            return $objResponse->access_token;
+        } catch (Exception $e) {
+            echo "Exception-".$e->getMessage();
+        }
+    }
+}
+
+try {
+    //Client ID of the application.
+    $clientID     = $argv[1];
+    //Client Secret key of the application.
+    $clientSecret = $argv[2];
+    //OAuth Url.
+    $authUrl      = "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13/";
+    //Application Scope Url
+    $scopeUrl     = "http://api.microsofttranslator.com";
+    //Application grant type
+    $grantType    = "client_credentials";
+
+    //Create the AccessTokenAuthentication object.
+    $authObj      = new AccessTokenAuthentication();
+    //Get the Access token.
+    $accessToken  = $authObj->getTokens($grantType, $scopeUrl, $clientID, $clientSecret, $authUrl);
+	print_r ($accessToken);
+} catch (Exception $e) {
+    echo "Exception: " . $e->getMessage() . PHP_EOL;
+}
