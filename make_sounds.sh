9be3ee5039def1761039b87b766672720e0434fd
Julian Pawlowski <julian.pawlowski@gmail.com>
2013-02-23 18:22:24 +0100

    several improvements

--- make_sounds.sh
+++ make_sounds.sh
@@ -1,91 +1,37 @@
 #!/bin/bash
+#
+# FreeSwitch
+# TTS Voice Prompt Generator
+#
+# Copyright (c) 2013, Julian Pawlowski <jp@jps-networks.eu>
+# See LICENSE file for details.
+#
 
 set -e
 
+# Check for needed tools
 if [[ ! -d ./input || ! curl || ! sox || ! mpg123 || ! perl || ! php ]]; then
 	echo -e "\nFATAL ERROR: Either one of the following errors occured:\n\n- input directory does not exist\n- curl/sox/mpg123/perl/php are not installed\n"
 	exit 1
 fi
 
-MARYTTSURL="https://tts.profhost.eu"
-MARYTTSVOICESDE="bits1-hsmm bits3 bits3-hsmm dfki-pavoque-neutral dfki-pavoque-neutral-hsmm"
-BING_OAUTH_CLIENTID=""
-BING_OAUTH_CLIENTSECRET=""
+# Read configuration if existing
+[ -e ./config ] && . ./config
+VERSION="`git tag | sort | tail -1`"
 
+# Search for voice text files
 FILES="`cd ./input; find . -name *.txt`"
 
-# MARY TTS
-if [[ x"$1" == x"marytts" || x"$1" == x"" ]]; then
-	echo -e "\nNOW PROCESSING WITH ENGINE: MARY TTS\n"
-	for FILE in $FILES; do
+##
+##
+## Create voice prompts via TTS
+##
+##
 
-		BASENAME="${FILE#.*/}"
-		FILENAME="${BASENAME%%.*}"
-		FILENAME_FLAT="${FILENAME#*/}"
-		LOCALE="${BASENAME%%/*}"
-		set +e
-		CHECK_TODO=`grep "#TODO" "./input/${BASENAME}"`
-		set -e
-
-		if [ x"${CHECK_TODO}" != x"" ]; then
-			echo "Ignoring ${FILENAME}"
-			continue;
-		fi
-
-		eval echo \${MARYTTSVOICES${LOCALE^^}} > fs_voices.tmp
-		VOICES="`cat fs_voices.tmp`"
-		rm -rf fs_voices.tmp
-
-		for VOICE in $VOICES; do
-	
-			OUTPUT_DIR="./output/${LOCALE}/tts/${VOICE}/${FILENAME_FLAT%/*}/16000"
-			OUTPUT_DIR8k="./output/${LOCALE}/tts/${VOICE}/${FILENAME_FLAT%/*}/8000"
-			OUTPUT_FILE="${OUTPUT_DIR}/${FILENAME##*/}.wav"
-			OUTPUT_FILE8k="${OUTPUT_DIR8k}/${FILENAME##*/}.wav"
-	
-			if [ -f "./input/${BASENAME}.maryxml" ]; then
-				INPUT_FILE="./input/${FILENAME}.maryxml"
-				INPUT_TYPE="RAWMARYXML"
-			else
-				INPUT_FILE="./input/${FILENAME}.txt"
-				INPUT_TYPE="TEXT"
-			fi
-	
-			if [ ! -f "${OUTPUT_FILE}" ]; then
-				echo -n "Processing ${FILENAME} for voice ${VOICE} ... "
-				mkdir -p "${OUTPUT_DIR}"
-				TEXT="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "`cat ${INPUT_FILE}`")"
-				curl -s "${MARYTTSURL}/process?INPUT_TEXT=${TEXT}&INPUT_TYPE=${INPUT_TYPE}&OUTPUT_TYPE=AUDIO&AUDIO=WAVE_FILE&LOCALE=${LOCALE}&VOICE=${VOICE}" > "${OUTPUT_FILE}"
-
-				set +e
-				CHECK_FILE="`file ${OUTPUT_FILE} | grep "WAVE audio"`"
-				if [ x"${CHECK_FILE}" == x"" ]; then
-					echo "FAILED"
-					rm -f "${OUTPUT_FILE}"
-				else
-					echo "ok"
-				 fi
-				 set -e
-			fi
-
-			if [[ -f "${OUTPUT_FILE}" ]]; then
-				echo "Improving ${FILENAME} ..."
-				sox "${OUTPUT_FILE}" "${OUTPUT_FILE}.imp.wav" silence 1 0.1 0.0% reverse
-				rm -f "${OUTPUT_FILE}"
-				sox "${OUTPUT_FILE}.imp.wav" "${OUTPUT_FILE}" silence 1 0.1 0.0% reverse
-				rm -f "${OUTPUT_FILE}.imp.wav"
-			fi
-	
-			if [[ ! -f "${OUTPUT_FILE8k}" && -f "${OUTPUT_FILE}" ]]; then
-				echo "Converting ${FILENAME} to 8kHz ..."
-				mkdir -p "${OUTPUT_DIR8k}"
-				sox -t wav "${OUTPUT_FILE}" -c1 -r8000 -b16 -e signed-integer "${OUTPUT_FILE8k}"
-			fi
-		done
-	done
-fi
+echo -e "\n\nFreeSwitch TTS Voice Prompt Generator v${VERSION}\n"
 
 # GOOGLE TTS
+#
 if [[ x"$1" == x"googletts" || x"$1" == x"" ]]; then
 	echo -e "\nNOW PROCESSING WITH ENGINE: GOOGLE TTS\n"
 	for FILE in $FILES; do
@@ -103,7 +49,7 @@ if [[ x"$1" == x"googletts" || x"$1" == x"" ]]; then
 			continue;
 		fi
 
-		OUTPUT_DIR_TMP="./output.tmp/${LOCALE}/tts/google/${FILENAME_FLAT%/*}"
+		OUTPUT_DIR_TMP="./cache/${LOCALE}/tts/google/${FILENAME_FLAT%/*}"
 		OUTPUT_DIR="./output/${LOCALE}/tts/google/${FILENAME_FLAT%/*}/16000"
 		OUTPUT_DIR8k="./output/${LOCALE}/tts/google/${FILENAME_FLAT%/*}/8000"
 		OUTPUT_FILE_TMP="${OUTPUT_DIR_TMP}/${FILENAME##*/}.mp3"
@@ -112,39 +58,76 @@ if [[ x"$1" == x"googletts" || x"$1" == x"" ]]; then
 		INPUT_FILE="./input/${FILENAME}.txt"
 	
 		if [ ! -f "${OUTPUT_FILE_TMP}" ]; then
-			echo -n "Processing ${FILENAME} ... "
+			echo -n "Processing ${FILENAME} ..."
 			mkdir -p "${OUTPUT_DIR_TMP}"
-			TEXT="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "`cat ${INPUT_FILE}`")"
-			curl -A "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17" -s "http://translate.google.com/translate_tts?tl=${LOCALE}&q=${TEXT}" > "${OUTPUT_FILE_TMP}"
 
-			set +e
-			CHECK_FILE="`file ${OUTPUT_FILE_TMP} | grep "MPEG"`"
-			if [ x"${CHECK_FILE}" == x"" ]; then
-				echo "FAILED"
-				rm -f "${OUTPUT_FILE_TMP}"
+			count=0
+			IFS="
+"
+			for LINE in `cat $INPUT_FILE`; do
+				if [ "${LINE}" == "" ]; then
+					continue
+				fi
+
+				count=$(( count + 1 ))
+				LINE_ENCODED="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "${LINE}")"
+
+				curl -A "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17" \
+					-s "http://translate.google.com/translate_tts?tl=${LOCALE}&q=${LINE_ENCODED}" > "${OUTPUT_FILE_TMP}.${count}.mp3"
+
+				if [ -e "${OUTPUT_FILE_TMP}.${count}.mp3" ]; then
+					set +e
+					CHECK_FILE="`file ${OUTPUT_FILE_TMP}.${count}.mp3 | grep "MPEG"`"
+					set -e
+					if [ x"${CHECK_FILE}" == x"" ]; then
+						echo " FAILED"
+						rm -f "${OUTPUT_FILE_TMP}.*"
+						break
+					else
+						echo -n " file${count}"
+					fi
+				else
+					echo " FAILED"
+					break
+				fi
+			done
+
+			if [ "${count}" == "1" ]; then
+				if [ -e "${OUTPUT_FILE_TMP}.1.mp3" ]; then
+				       mv -f "${OUTPUT_FILE_TMP}.1.mp3" "${OUTPUT_FILE_TMP}"
+				       echo " OK"
+			       else
+				       echo " FAILED"
+			       fi
 			else
-				echo "ok"
-			 fi
-			 set -e
+				count2=2
+				cat "${OUTPUT_FILE_TMP}.1.mp3" > "${OUTPUT_FILE_TMP}"
+				rm -f "${OUTPUT_FILE_TMP}.1.mp3"
+				while [ ${count2} -le ${count} ]; do
+					cat "${OUTPUT_FILE_TMP}.${count2}.mp3" >> "${OUTPUT_FILE_TMP}"
+					rm -f "${OUTPUT_FILE_TMP}.${count2}.mp3"
+					echo " OK"
+					count2=$(( count2 + 1 ))
+				done
+			fi
 		fi
 
 		if [[ ! -f "${OUTPUT_FILE}" && -f "${OUTPUT_FILE_TMP}" ]]; then
-			echo "Converting ${FILENAME} to 16kHz ..."
+			echo "  > Converting to 16kHz ..."
 			mkdir -p "${OUTPUT_DIR}"
 			mpg123 -q -w ${OUTPUT_FILE} ${OUTPUT_FILE_TMP}
-		fi
-
-		if [[ -f "${OUTPUT_FILE}" ]]; then
-			echo "Improving ${FILENAME} ..."
-			sox "${OUTPUT_FILE}" "${OUTPUT_FILE}.imp.wav" silence 1 0.1 0.0% reverse
-			rm -f "${OUTPUT_FILE}"
-			sox "${OUTPUT_FILE}.imp.wav" "${OUTPUT_FILE}.imp2.wav" silence 1 0.1 0.0% reverse
-			sox "${OUTPUT_FILE}.imp2.wav" "${OUTPUT_FILE}" tempo 1.25
-			rm -f "${OUTPUT_FILE}.imp.wav" "${OUTPUT_FILE}.imp2.wav"
+			if [[ -f "${OUTPUT_FILE}" ]]; then
+				echo "  > Optimizing ..."
+				sox "${OUTPUT_FILE}" "${OUTPUT_FILE}.imp.wav" silence 1 0.1 0.0% reverse
+				rm -f "${OUTPUT_FILE}"
+				sox "${OUTPUT_FILE}.imp.wav" "${OUTPUT_FILE}.imp2.wav" silence 1 0.1 0.0% reverse
+				sox "${OUTPUT_FILE}.imp2.wav" "${OUTPUT_FILE}" tempo 1.25
+				rm -f "${OUTPUT_FILE}.imp.wav" "${OUTPUT_FILE}.imp2.wav"
+			fi
 		fi
 
 		if [[ ! -f "${OUTPUT_FILE8k}" && -f "${OUTPUT_FILE}" ]]; then
-			echo "Converting ${FILENAME} to 8kHz ..."
+			echo "  > Converting to 8kHz ..."
 			mkdir -p "${OUTPUT_DIR8k}"
 			sox -t wav "${OUTPUT_FILE}" -c1 -r8000 -b16 -e signed-integer "${OUTPUT_FILE8k}"
 		fi
@@ -152,9 +135,10 @@ if [[ x"$1" == x"googletts" || x"$1" == x"" ]]; then
 fi
 
 # BING TTS
+#
 if [[ x"$1" == x"bingtts" || x"$1" == x"" ]]; then
 	if [[ x"${BING_OAUTH_CLIENTID}" != x"" && x"${BING_OAUTH_CLIENTSECRET}" != x"" ]]; then
-		echo -e "\nNOW PROCESSING WITH ENGINE: BING TTS\n"
+		echo -e "\n\nNOW PROCESSING WITH ENGINE: BING TTS\n"
 		
 		for FILE in $FILES; do
 			BASENAME="${FILE#.*/}"
@@ -170,7 +154,7 @@ if [[ x"$1" == x"bingtts" || x"$1" == x"" ]]; then
 				continue;
 			fi
 
-			OUTPUT_DIR_TMP="./output.tmp/${LOCALE}/tts/bing/${FILENAME_FLAT%/*}"
+			OUTPUT_DIR_TMP="./cache/${LOCALE}/tts/bing/${FILENAME_FLAT%/*}"
 			OUTPUT_DIR="./output/${LOCALE}/tts/bing/${FILENAME_FLAT%/*}/16000"
 			OUTPUT_DIR8k="./output/${LOCALE}/tts/bing/${FILENAME_FLAT%/*}/8000"
 			OUTPUT_FILE_TMP="${OUTPUT_DIR_TMP}/${FILENAME##*/}.wav"
@@ -182,22 +166,61 @@ if [[ x"$1" == x"bingtts" || x"$1" == x"" ]]; then
 				echo -n "Processing ${FILENAME} ... "
 				BING_OAUTH_TOKEN="`php oauth_bingtts.php "${BING_OAUTH_CLIENTID}" "${BING_OAUTH_CLIENTSECRET}"`"
 				mkdir -p "${OUTPUT_DIR_TMP}"
-				TEXT="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "`cat ${INPUT_FILE}`")"
-				curl -A "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17" -H "Authorization: Bearer ${BING_OAUTH_TOKEN}" -s "http://api.microsofttranslator.com/V2/Http.svc/Speak?language=${LOCALE}&format=audio/wav&options=MaxQuality&appid=&text=${TEXT}" > "${OUTPUT_FILE_TMP}"
 
-				set +e
-				CHECK_FILE="`file ${OUTPUT_FILE_TMP} | grep "WAVE audio"`"
-				if [ x"${CHECK_FILE}" == x"" ]; then
-					echo "FAILED"
-					rm -f "${OUTPUT_FILE_TMP}"
+				count=0
+				IFS="
+"
+				for LINE in `cat $INPUT_FILE`; do
+					if [ "${LINE}" == "" ]; then
+						continue
+					fi
+
+					count=$(( count + 1 ))
+					LINE_ENCODED="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "${LINE}")"
+
+					curl -A "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17" \
+						-H "Authorization: Bearer ${BING_OAUTH_TOKEN}" \
+						-s "http://api.microsofttranslator.com/V2/Http.svc/Speak?language=${LOCALE}&format=audio/wav&options=MaxQuality&appid=&text=${LINE_ENCODED}" > "${OUTPUT_FILE_TMP}.${count}.wav"
+
+					if [ -e "${OUTPUT_FILE_TMP}.${count}.wav" ]; then
+						set +e
+						CHECK_FILE="`file ${OUTPUT_FILE_TMP}.${count}.wav | grep "WAVE audio"`"
+						set -e
+						if [ x"${CHECK_FILE}" == x"" ]; then
+							echo " FAILED"
+							rm -f "${OUTPUT_FILE_TMP}.*"
+							break
+						else
+							echo -n " file${count}"
+						fi
+					else
+						echo " FAILED"
+						break
+					fi
+				done
+
+				if [ "${count}" == "1" ]; then
+					if [ -e "${OUTPUT_FILE_TMP}.1.wav" ]; then
+						mv -f "${OUTPUT_FILE_TMP}.1.wav" "${OUTPUT_FILE_TMP}"
+						echo " OK"
+					else
+						echo " FAILED"
+					fi
 				else
-					echo "ok"
-				 fi
-				 set -e
+					count2=2
+					FILE_LIST="${OUTPUT_FILE_TMP}.1.wav "
+					while [ ${count2} -le ${count} ]; do
+						FILE_LIST="${FILE_LIST} ${OUTPUT_FILE_TMP}.${count2}.wav"
+						count2=$(( count2 + 1 ))
+					done
+					sox ${OUTPUT_FILE_TMP}.* "${OUTPUT_FILE_TMP}"
+					rm -f ${OUTPUT_FILE_TMP}.*
+					echo " OK"
+				fi
 			fi
 
-			if [[ -f "${OUTPUT_FILE_TMP}" ]]; then
-				echo "Improving ${FILENAME} ..."
+			if [[ ! -f "${OUTPUT_FILE}" && -f "${OUTPUT_FILE_TMP}" ]]; then
+				echo "  > Optimizing ..."
 				mkdir -p "${OUTPUT_DIR}"
 				sox "${OUTPUT_FILE_TMP}" "${OUTPUT_FILE}.imp.wav" silence 1 0.1 0.0% reverse
 				sox "${OUTPUT_FILE}.imp.wav" "${OUTPUT_FILE}" silence 1 0.1 0.0% reverse
@@ -205,13 +228,13 @@ if [[ x"$1" == x"bingtts" || x"$1" == x"" ]]; then
 			fi
 
 			if [[ ! -f "${OUTPUT_FILE8k}" && -f "${OUTPUT_FILE}" ]]; then
-				echo "Converting ${FILENAME} to 8kHz ..."
+				echo "  > Converting to 8kHz ..."
 				mkdir -p "${OUTPUT_DIR8k}"
 				sox -t wav "${OUTPUT_FILE}" -c1 -r8000 -b16 -e signed-integer "${OUTPUT_FILE8k}"
 			fi
 		done
 	else
-		echo "OAuth API credentials for BING missing. See http://msdn.microsoft.com/en-us/library/hh454950.aspx"
+		echo "Note: OAuth API credentials for BING missing in config file. See http://msdn.microsoft.com/en-us/library/hh454950.aspx"
 	fi
 fi
 
@@ -225,8 +248,9 @@ cd ./output
 for VOICE in `find . -type d -depth 3`; do
 	FILENAME="`echo ${VOICE:1} | sed -e 's/\//-/g'`"
 	echo "freeswitch-sounds${FILENAME}-16000"
-	find "$VOICE" -name '16000' -type d | xargs tar cfpz ../freeswitch-sounds${FILENAME}-16000-0.0.6.tar.gz
+	find "$VOICE" -name '16000' -type d | xargs tar cfpz ../freeswitch-sounds${FILENAME}-16000-${VERSION}.tar.gz
 	echo "freeswitch-sounds${FILENAME}-8000"
-	find "$VOICE" -name '8000' -type d | xargs tar cfpz ../freeswitch-sounds${FILENAME}-8000-0.0.6.tar.gz
+	find "$VOICE" -name '8000' -type d | xargs tar cfpz ../freeswitch-sounds${FILENAME}-8000-${VERSION}.tar.gz
 done
 cd ..
+
