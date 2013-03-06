ff78f2267bda8464c2fd54495e5dc144dfce5905
Julian Pawlowski <julian.pawlowski@gmail.com>
2013-03-06 19:23:16 +0100

    fix

--- make_sounds.sh
+++ make_sounds.sh
@@ -143,7 +143,7 @@ if [[ x"$1" == x"googletts" ]]; then
 			mpg123 -q -w ${OUTPUT_FILE} ${OUTPUT_FILE_TMP}
 			if [[ -f "${OUTPUT_FILE}" ]]; then
 				echo "  > Optimizing ..."
-				sox "${OUTPUT_FILE}" -n stat -v 2> "${OUTPUT_FILE}.volc"
+				sox "${OUTPUT_FILE}" -n stat 2> "${OUTPUT_FILE}.volc"
 				MAXVOL="`cat "${OUTPUT_FILE}.volc" | grep "Volume adjustment" | cut -d ':' -f2 | tr -d ' '`"
 				[ x"${MAXVOL}" != x"" ] && sox -v `echo ${MAXVOL}-0.15 | bc` "${OUTPUT_FILE}" "${OUTPUT_FILE}.volmax.wav" || cp "${OUTPUT_FILE}" "${OUTPUT_FILE}.volmax.wav"
 				rm -f "${OUTPUT_FILE}"
@@ -263,9 +263,9 @@ if [[ x"$1" == x"bingtts" ]]; then
 			if [[ ! -f "${OUTPUT_FILE}" && -f "${OUTPUT_FILE_TMP}" ]]; then
 				echo "  > Optimizing ..."
 				mkdir -p "${OUTPUT_DIR}"
-				sox "${OUTPUT_FILE_TMP}" -n stat -v 2> "${OUTPUT_FILE}.volc"
+				sox "${OUTPUT_FILE_TMP}" -n stat 2> "${OUTPUT_FILE}.volc"
 				MAXVOL="`cat "${OUTPUT_FILE}.volc" | grep "Volume adjustment" | cut -d ':' -f2 | tr -d ' '`"
-				[ x"${MAXVOL}" != x"" ] && sox -v `echo ${MAXVOL}-0.15 | bc` "${OUTPUT_FILE_TMP}" "${OUTPUT_FILE}.volmax.wav" || cp "${OUTPUT_FILE}" "${OUTPUT_FILE}.volmax.wav"
+				[ x"${MAXVOL}" != x"" ] && sox -v `echo ${MAXVOL}-0.15 | bc` "${OUTPUT_FILE_TMP}" "${OUTPUT_FILE}.volmax.wav" || cp "${OUTPUT_FILE_TMP}" "${OUTPUT_FILE}.volmax.wav"
 				sox "${OUTPUT_FILE}.volmax.wav" "${OUTPUT_FILE}.imp.wav" silence 1 0.1 0.0% reverse
 				sox "${OUTPUT_FILE}.imp.wav" "${OUTPUT_FILE}" silence 1 0.1 0.0% reverse
 				rm -f "${OUTPUT_FILE}.volc" "${OUTPUT_FILE}.volmax.wav" "${OUTPUT_FILE}.imp.wav"
